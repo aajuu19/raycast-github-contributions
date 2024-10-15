3b263d6e15db8ffe45b56b5e2db578c8164d6ee2
aajuu19 <aaronjung19@gmail.com>
2024-10-15 23:04:11 +0200

    display actual github data

--- src/show-github-stats.tsx
+++ src/show-github-stats.tsx
@@ -1,6 +1,9 @@
 import { MenuBarExtra } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
+import { format, startOfMonth } from "date-fns";
 import { TContributionResponse } from "./types/common";
+import { getContributionsByDate } from "./utils/getContributionsByDate";
+import { calculateStreak } from "./utils/calculateStreak";
 
 const baseQuery = `
 {
@@ -30,21 +33,42 @@ export default function Command() {
     body: JSON.stringify({ query: baseQuery }),
   });
 
+  const contributionCalendar = data?.data.user.contributionsCollection.contributionCalendar;
+
+  if (error || !data || !contributionCalendar) return null;
+
+  const contributionDays = contributionCalendar.weeks.flatMap((week) => week.contributionDays);
+
+  const today = format(new Date(), "yyyy-MM-dd");
+  const currentMonth = format(startOfMonth(new Date()), "yyyy-MM-dd");
+  const past30Days = format(new Date(new Date().setDate(new Date().getDate() - 30)), "yyyy-MM-dd");
+  const oneYearAgo = format(new Date(new Date().setFullYear(new Date().getFullYear() - 1)), "yyyy-MM-dd");
+
+  // Contributions for today
+  const todayContributions = getContributionsByDate(contributionDays, (date) => date === today);
+
+  // Contributions for this month
+  const thisMonthContributions = getContributionsByDate(contributionDays, (date) => date >= currentMonth);
+
+  // Contributions for the past 30 days
+  const past30DaysContributions = getContributionsByDate(contributionDays, (date) => date >= past30Days);
+
+  // Contributions for the past 365 days
+  const yearContributions = getContributionsByDate(contributionDays, (date) => date >= oneYearAgo);
+
+  const { currentStreak, longestStreak } = calculateStreak(contributionDays);
+
   return (
-    <MenuBarExtra tooltip="Your Pull Requests" title="🔥 1 - 🍎 3">
+    <MenuBarExtra tooltip="Your contribution stats" title={`🔥 ${currentStreak} - 🍎 ${longestStreak}`}>
       <MenuBarExtra.Section title="Streaks">
-        <MenuBarExtra.Item
-          title={`🔥 ${isLoading ? "🕸️" : data?.data.user.contributionsCollection.contributionCalendar.totalContributions}`}
-          subtitle=" - Contribution Streak"
-          onAction={() => {}}
-        />
-        <MenuBarExtra.Item title="🍎 4" subtitle=" - Longest Streak" onAction={() => {}} />
+        <MenuBarExtra.Item title={`🔥 ${currentStreak}`} subtitle=" - Current Streak" onAction={() => {}} />
+        <MenuBarExtra.Item title={`🍎 ${longestStreak}`} subtitle=" - Longest Streak" onAction={() => {}} />
       </MenuBarExtra.Section>
       <MenuBarExtra.Section title="Contribution stats">
-        <MenuBarExtra.Item title="👨🏻‍💻7" subtitle=" - Today" onAction={() => {}} />
-        <MenuBarExtra.Item title="💼 28" subtitle=" - This month" onAction={() => {}} />
-        <MenuBarExtra.Item title="🗓️ 30" subtitle=" - Last 30 days" onAction={() => {}} />
-        <MenuBarExtra.Item title="📊 106" subtitle=" - Last 365 days" onAction={() => {}} />
+        <MenuBarExtra.Item title={`👨🏻‍💻 ${todayContributions}`} subtitle=" - Today" onAction={() => {}} />
+        <MenuBarExtra.Item title={`💼 ${thisMonthContributions}`} subtitle=" - This month" onAction={() => {}} />
+        <MenuBarExtra.Item title={`🗓️ ${past30DaysContributions}`} subtitle=" - Last 30 days" onAction={() => {}} />
+        <MenuBarExtra.Item title={`📊 ${yearContributions}`} subtitle=" - Last 365 days" onAction={() => {}} />
       </MenuBarExtra.Section>
     </MenuBarExtra>
   );
